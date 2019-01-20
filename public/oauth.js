1e3924f03070022eae1e5dc49f5080a797e417fa
mjtr <mjtr1995@gmail.com>
2019-01-20 18:26:50 +0100

    s

--- public/oauth.js
+++ public/oauth.js
@@ -5,15 +5,23 @@ angular.module("managerApp").controller("oauthCtrl", ["$scope", "$http", functio
 
   function onSignIn(googleUser) {
     var profile = googleUser.getBasicProfile();
-    console.log('ID: ' + profile.getId());
-    console.log('Name: ' + profile.getName());
-    console.log('Image URL: ' + profile.getImageUrl());
-    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+    $(".g-signin2").css("display","one");
+    $(".data").css("display","block");
+    $("#pic").attr('src', profile.getImageUrl());
+    $("email").text(profile.getEmail());
+
+    //console.log('ID: ' + profile.getId());
+    //console.log('Name: ' + profile.getName());
+    //console.log('Image URL: ' + profile.getImageUrl());
+    //console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
   }
 
   function signOut() {
     var auth2 = gapi.auth2.getAuthInstance();
     auth2.signOut().then(function () {
+      alert("you have been succesfully  signed out");
+      $(".g-signin2").css("display","one");
+      $(".data").css("display","none");
       console.log('User signed out.');
     });
 }
