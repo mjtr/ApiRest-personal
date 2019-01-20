98070cb07ba8c746beb27e8667ade1c54a59a892
mjtr <mjtr1995@gmail.com>
2019-01-20 20:39:24 +0100

    s

--- public/oauth.js
+++ public/oauth.js
@@ -6,10 +6,10 @@
     $("#pic").attr('src', profile.getImageUrl());
     $("email").text(profile.getEmail());
 
-    //console.log('ID: ' + profile.getId());
-    //console.log('Name: ' + profile.getName());
-    //console.log('Image URL: ' + profile.getImageUrl());
-    //console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+    console.log('ID: ' + profile.getId());
+    console.log('Name: ' + profile.getName());
+    console.log('Image URL: ' + profile.getImageUrl());
+    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
   }
 
   function signOut() {
