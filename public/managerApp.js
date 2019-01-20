9226601fb3a5e8cfbbd4c18055baeaa3dffe88b1
mjtr <mjtr1995@gmail.com>
2019-01-20 15:27:10 +0100

    oauth preparations ok

--- public/managerApp.js
+++ public/managerApp.js
@@ -1,87 +1,92 @@
 /*global angular*/
 
 angular.module("managerApp",["ngRoute", 'angularUtils.directives.dirPagination']).config(function($routeProvider){
-    
+
     $routeProvider
         .when("/", {
-            
-           templateUrl: "/home.html" 
-            
+
+           templateUrl: "/home.html"
+
         }).when("/api/v2/rape-stats",{
-            
+
+            templateUrl: "/oauth.html",
+            controller: "oauthCtrl"
+
+        }).when("/oauth",{
+
             templateUrl: "/rape-manager/front-end/normal/rapeList.html",
             controller: "rapeListCtrl"
-            
+
         }).when("/api/v2/rape-stats/edit/:country/:year", {
-            
+
             templateUrl: "/rape-manager/front-end/normal/rapeEdit.html",
             controller: "rapeEditCtrl"
-            
+
         }).when("/api/v2/secure/rape-stats",{
-            
+
             templateUrl: "/rape-manager/front-end/secure/rapeSecureList.html",
             controller: "rapeSecureListCtrl"
-            
-            
+
+
         }).when("/api/v2/secure/rape-stats/edit/:country/:year", {
-            
+
             templateUrl: "/rape-manager/front-end/secure/rapeSecureEdit.html",
             controller: "rapeSecureEditCtrl"
-            
+
         }).when("/analytics" , {
-            
+
             templateUrl: "/graphics.html"
-        
+
         }).when("/analytics/rape-stats/geo",{
-            
+
         templateUrl: "/rape-manager/analytics/rapeGeo.html",
         controller: "rapeGeoCtrl"
-        
-            
+
+
         }).when("/analytics/rape-stats/high", {
-                      
+
            templateUrl: "/rape-manager/analytics/rapeHigh.html",
            controller: "rapeHighCtrl"
-           
-            
+
+
         }).when("/analytics/rape-stats/rgraph",{
-            
+
             templateUrl: "/rape-manager/analytics/rapeRGraph.html",
             controller: "rapeRGraphCtrl"
-            
+
         }).when("/analytics/proxyRD",{
-            
+
             templateUrl: "/rape-manager/analytics/integrations/proxyRD.html",
             controller: "proxyRDCtrl"
-            
+
         }).when("/analytics/normalRW",{
-            
+
             templateUrl: "/rape-manager/analytics/integrations/normalRW.html",
             controller: "normalRWCtrl"
-            
+
         }).when("/analytics/mashape1",{
-            
+
             templateUrl: "/rape-manager/analytics/integrations/mashape1.html",
             controller: "mashape1Ctrl"
-            
+
         }).when("/analytics/mashape2",{
-            
+
             templateUrl: "/rape-manager/analytics/integrations/mashape2.html",
             controller: "mashape2Ctrl"
-            
+
         }).when("/analytics/progweb1",{
-            
+
             templateUrl: "/rape-manager/analytics/integrations/ProgWeb1.html",
             controller: "ProgWeb1Ctrl"
-            
+
         }).when("/analytics/progweb2",{
-            
+
             templateUrl: "/rape-manager/analytics/integrations/ProgWeb2.html",
             controller: "ProgWeb2Ctrl"
-            
+
         });
-        
-        
-        
-    
+
+
+
+
 }) ;
---
+++ public/oauth.js
@@ -0,0 +1,8 @@
+/*global angular*/
+/*global OAuth*/
+
+angular.module("managerApp").controller("oauthCtrl", ["$scope", "$http", function($scope, $http) {
+
+
+
+}]);
