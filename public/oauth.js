357547988a65a06b3b345f47b5d18c54f41360ae
mjtr <mjtr1995@gmail.com>
2019-01-20 18:05:16 +0100

    last changse

--- public/oauth.js
+++ public/oauth.js
@@ -3,6 +3,19 @@
 
 angular.module("managerApp").controller("oauthCtrl", ["$scope", "$http", function($scope, $http) {
 
+  function onSignIn(googleUser) {
+    var profile = googleUser.getBasicProfile();
+    console.log('ID: ' + profile.getId());
+    console.log('Name: ' + profile.getName());
+    console.log('Image URL: ' + profile.getImageUrl());
+    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+  }
 
+  function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+      console.log('User signed out.');
+    });
+}
 
 }]);
