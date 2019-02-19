fe693168fdc2750b7c444ff0256c8c178d3bd47b
mjtr <mjtr1995@gmail.com>
2019-02-19 13:51:32 +0100

    align tests

--- test/rapeConfig.js
+++ test/rapeConfig.js
@@ -1,11 +1,11 @@
 exports.config = {
-    
-    seleniumAddress: 'http://localhost:8910',
-    
-    specs: ['T01-rapeLoadData.js' , 'T02-rapeCreateData.js'],
-    
-    capabilities: {
-        'browserName' : 'phantom.js'
-    }
-    
+
+  seleniumAddress: 'http://localhost:8910',
+
+  specs: ['T01-rapeLoadData.js', 'T02-rapeCreateData.js'],
+
+  capabilities: {
+    'browserName': 'phantom.js'
+  }
+
 };
